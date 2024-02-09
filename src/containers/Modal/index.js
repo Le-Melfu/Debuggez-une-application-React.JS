3794e12782f1ec526289fe2d872ce83452b2882a
Melfu <degardenzi.clement@gmail.com>
2024-02-09 18:07:10 +0100

    end day

--- src/containers/Modal/index.js
+++ src/containers/Modal/index.js
@@ -1,39 +1,42 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
-import Icon from "../../components/Icon";
-import "./style.scss";
+import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
+import Icon from '../../components/Icon'
+import './style.scss'
 
 const Modal = ({ opened, Content, children }) => {
-  const [isOpened, setIsOpened] = useState(opened);
-  return (
-    <>
-      {children({ isOpened, setIsOpened })}
-      {isOpened && (
-        <div className="modal">
-          <div className="content">
-            {Content}
-            <button
-              type="button"
-              data-testid="close-modal"
-              onClick={() => setIsOpened(false)}
-            >
-              <Icon name="close" />
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+    const [isOpened, setIsOpened] = useState(opened)
+    useEffect(() => {
+        setIsOpened(opened) // Mettre à jour l'état isOpened lorsque la prop opened change
+    }, [opened])
+    return (
+        <>
+            {children({ isOpened, setIsOpened })}
+            {isOpened && (
+                <div className="modal">
+                    <div className="content">
+                        {Content}
+                        <button
+                            type="button"
+                            data-testid="close-modal"
+                            onClick={() => setIsOpened(false)}
+                        >
+                            <Icon name="close" />
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
 
 Modal.defaultProps = {
-  opened: false,
+    opened: false,
 }
 
 Modal.propTypes = {
-  opened: PropTypes.bool,
-  Content: PropTypes.node.isRequired,
-  children: PropTypes.func.isRequired,
+    opened: PropTypes.bool,
+    Content: PropTypes.node.isRequired,
+    children: PropTypes.func.isRequired,
 }
 
-export default Modal;
+export default Modal
